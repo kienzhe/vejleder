8b4b68f3d92488bc9a75bf8984903b85e007c082
mardix2503 <93321382+mardix2503@users.noreply.github.com>
2022-11-21 12:24:44 +0100

    Update registration-2.php

--- registration-2.php
+++ registration-2.php
@@ -14,7 +14,7 @@
 
 </head>
 
-  <body>
+<body>
     <nav>
         <ul id="navbar">
             <li class="navbar_link"><a href="#">Registration</a></li>
@@ -25,7 +25,7 @@
     </nav>
 
     <div id="content">
-      <div class="sidebar">
+        <div class="sidebar">
             <ul class="steps">
                 <li class="steps_link"><a href="#">Del 1</a></li>
                 <li class="steps_link"><a href="#">Del 2</a></li>
@@ -70,117 +70,159 @@
 
             <section id="section2">
                 <h2>Emne</h2> 
-                <input class="drop_down" type="text" placeholder="Emne" name="search">
+                <input class="search" type="text" placeholder="Emne" name="search">
 
                 <form class="subject_grid">   
-                    <div class="subject_grid_item_1">                 
+                    <div class="subject_grid_item_0">                 
                         <input type="checkbox" id="Administrative forhold " name="subject" value="Administrative forhold ">
-                            <label for="Administrative forhold ">Administrative forhold </label>
+                            <label for="Administrative forhold ">Administrative forhold</label>
+
+                            <img class="info" id="info_0" src="img/info_icon.png" alt="info">
+                    </div>
+
+                    <div class="subject_grid_item_1">
+                        <input type="checkbox" id="Barsel" name="subject" value="Barsel">
+                            <label for="Barsel">Barsel</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_2">
-                        <input type="checkbox" id="Barsel" name="subject" value="Barsel">
-                            <label for="Barsel">Barsel</label>
+                        <input type="checkbox" id="Eksamen" name="subject" value="Eksamen">
+                            <label for="Eksamen">Eksamen</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_3">
-                        <input type="checkbox" id="Eksamen" name="subject" value="Eksamen">
-                            <label for="Eksamen">Eksamen</label>
+                        <input type="checkbox" id="Fastholde trivsel" name="subject" value="Fastholde trivsel">
+                            <label for="Fastholde trivsel">Fastholde trivsel</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_4">
-                        <input type="checkbox" id="Fastholde trivsel" name="subject" value="Fastholde trivsel">
-                            <label for="Fastholde trivsel">Fastholde trivsel</label>
+                        <input type="checkbox" id="Internationale muligheder" name="subject" value="Internationale muligheder">
+                            <label for="Internationale muligheder">Internationale muligheder</label>
+                        
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_5">
-                        <input type="checkbox" id="Internationale muligheder" name="subject" value="Internationale muligheder">
-                            <label for="Internationale muligheder">Internationale muligheder</label>
+                        <input type="checkbox" id="Mistrivsel" name="subject" value="Mistrivsel">
+                            <label for="Mistrivsel">Mistrivsel</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_6">
-                        <input type="checkbox" id="Mistrivsel" name="subject" value="Mistrivsel">
-                            <label for="Mistrivsel">Mistrivsel</label>
+                        <input type="checkbox" id="Optagelsesvejledning" name="subject" value="Optagelsesvejledning">
+                            <label for="Optagelsesvejledning">Optagelsesvejledning</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_7">
-                        <input type="checkbox" id="Optagelsesvejledning" name="subject" value="Optagelsesvejledning">
-                            <label for="Optagelsesvejledning">Optagelsesvejledning</label>
+                        <input type="checkbox"id="Orlov" name="subject" value="Orlov">
+                            <label for="Orlov">Orlov</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_8">
-                        <input type="checkbox"id="Orlov" name="subject" value="Orlov">
-                            <label for="Orlov">Orlov</label>
+                        <input type="checkbox" id="Overflytning/genindskrivning" name="subject" value="Overflytning/genindskrivning">
+                            <label for="Overflytning/genindskrivning">Overflytning/genindskrivning</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_9">
-                        <input type="checkbox" id="Overflytning/genindskrivning" name="subject" value="Overflytning/genindskrivning">
-                            <label for="Overflytning/genindskrivning">Overflytning/genindskrivning</label>
+                        <input type="checkbox" id="Personlige forhold" name="subject" value="Personlige forhold">
+                            <label for="Personlige forhold">Personlige forhold</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_10">
-                        <input type="checkbox" id="Personlige forhold" name="subject" value="Personlige forhold">
-                            <label for="Personlige forhold">Personlige forhold</label>
+                        <input type="checkbox" id="E-handel" name="subject" value="E-handel">
+                            <label for="E-handel">E-handel</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_11">
-                        <input type="checkbox" id="E-handel" name="subject" value="E-handel">
-                            <label for="E-handel">E-handel</label>
+                        <input type="checkbox" id="Praktik i DK/klinik/dialogmøder" name="subject" value="Praktik i DK/klinik/dialogmøder">
+                            <label for="Praktik i DK/klinik/dialogmøder">Praktik i DK/klinik/dialogmøder</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_12">
-                        <input type="checkbox" id="Praktik i DK/klinik/dialogmøder" name="subject" value="Praktik i DK/klinik/dialogmøder">
-                            <label for="Praktik i DK/klinik/dialogmøder">Praktik i DK/klinik/dialogmøder</label>
+                        <input type="checkbox" id="Ikke studievejledningsrelevant henvendelse" name="subject" value="Ikke studievejledningsrelevant henvendelse">
+                            <label for="Ikke studievejledningsrelevant henvendelse">Ikke studievejledningsrelevant henvendelse</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_13">
-                        <input type="checkbox" id="Ikke studievejledningsrelevant henvendelse" name="subject" value="Ikke studievejledningsrelevant henvendelse">
-                            <label for="Ikke studievejledningsrelevant henvendelse">Ikke studievejledningsrelevant henvendelse</label>
-                    </div>
-
-                    <div class="subject_grid_item_14">
                         <input type="checkbox" id="SPS" name="subject" value="SPS">
                             <label for="SPS">SPS</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
                     
-                    <div class="subject_grid_item_15">
+                    <div class="subject_grid_item_14">
                         <input type="checkbox" id="Studieophør /-stop " name="subject" value="Studieophør /-stop ">
                             <label for="Studieophør /-stop ">Studieophør /-stop </label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
+                    </div>
+
+                    <div class="subject_grid_item_15">
+                        <input type="checkbox" id="Studieplanlægning " name="subject" value="Studieplanlægning ">
+                            <label for="Studieplanlægning ">Studieplanlægning </label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_16">
-                        <input type="checkbox" id="Studieplanlægning " name="subject" value="Studieplanlægning ">
-                            <label for="Studieplanlægning ">Studieplanlægning </label>
-                    </div>
-
-                    <div class="subject_grid_item_17">
                         <input type="checkbox" id="Studietvivl / Studievalg" name="subject" value="Studietvivl / Studievalg">
                             <label for="Studietvivl / Studievalg">Studietvivl / Studievalg</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
                     
+                    <div class="subject_grid_item_17">
+                        <input type="checkbox" id="Sygdom  " name="subject" value="Sygdom">
+                            <label for="Sygdom  ">Sygdom</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
+                    </div>
+
                     <div class="subject_grid_item_18">
-                        <input type="checkbox" id="Sygdom  " name="subject" value="Sygdom  ">
-                            <label for="Sygdom  ">Sygdom  </label>
+                        <input type="checkbox" id="Undervisningen" name="subject" value="Undervisningen">
+                            <label for="Undervisningen">Undervisningen</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
 
                     <div class="subject_grid_item_19">
-                        <input type="checkbox" id="Undervisningen" name="subject" value="Undervisningen">
-                            <label for="Undervisningen">Undervisningen</label>
-                    </div>
-
-                    <div class="subject_grid_item_20">
                         <input type="checkbox" id="Økonomi" name="subject" value="Økonomi">
                             <label for="Økonomi">Økonomi</label>
+                                                    
+                        <img class="info" id="info_0" src="img/info_icon.png" alt="info">
                     </div>
                 </form>
 
-                <div class="buttons">
-                    <button id="back_button"><a href="registration-1.php">Tilbage</a></button>
+                <div class="buttons_box">
+                    <div class="buttons">
+                        <button id="back_button"><a href="registration-1.php">Tilbage</a></button>
 
-                    <button id= "registrate"></button>
+                        <button id= "registrate"><a href="#">Registere</a></button>
+                    </div>
                 </div>
             </section>
         </div> 
     </div>
-  </body>
+</body>
 </html>
