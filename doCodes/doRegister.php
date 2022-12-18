b0a8e79dc8d7c96dc76a5c058006680ac17744df
kienzhe <74898581+kienzhe@users.noreply.github.com>
2022-12-18 09:42:01 +0100

    Update doRegister.php

--- doCodes/doRegister.php
+++ doCodes/doRegister.php
@@ -16,14 +16,16 @@ foreach($topics as $key => $topic){
         $topic_string .= ";";
     }   
 }
-$user = $_SESSION['idUser'];
-$sql = "INSERT INTO registration (year, month, conversation, person_type, inquiry_type, educations_id, campus_id, counselor_id, topics_id)
-VALUES ('$year', '$month', '$conversation', '$person_type', '$inquiry', '$education', '$campus', '$user', '$topic_string')";
 
+$user = $_SESSION['idUser'];
+$danishMonths = [1 => 'Januar', 2 => 'Februar', 3 => 'Marts', 4 => 'April', 5 => 'Maj', 6 => 'Juni', 7 => 'Juli', 8 => 'August', 9 => 'September', 10 => 'Oktober', 11 => 'November', 12 => 'December'];
+
+$sql = "INSERT INTO registration (year, month_num, month, conversation, person_type, inquiry_type, educations_id, campus_id, counselor_id, topics_id)
+VALUES ('$year', '$month', '$danishMonths[$month]', '$conversation', '$person_type', '$inquiry', '$education', '$campus', '$user', '$topic_string')";
 
 if ($dbCon->query($sql) === TRUE) {
-      header('LOCATION:/vejleder/registration.php?s=registration'); 
+        header('LOCATION:/vejleder/registration.php?s=registration'); 
 } else {
-          header('LOCATION:/vejleder/registration.php?f=registration');
+        header('LOCATION:/vejleder/registration.php?f=registration');
 }
 ?>
