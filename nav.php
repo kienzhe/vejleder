b800eb4c15865402a72f2426ab0785526c774449
kienzhe <74898581+kienzhe@users.noreply.github.com>
2022-12-18 09:35:27 +0100

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,15 +2,16 @@
 include("includes/dbcon.php");
 $loginId = $_SESSION['idUser'];
 $counselor = "select first_name, last_name from counselor where login_id = '$loginId' ";
-$result = $dbCon->query($counselor);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$resultCouns = $dbCon->query($counselor);
+$row = mysqli_fetch_array($resultCouns, MYSQLI_ASSOC);
 $_SESSION['first_name'] = $row['first_name'];
 ?>
  <nav>
+     <div id="navwrapper">
         <ul id="navbar">
-            <li class="navbar_link"><a href="#">Registration</a></li>
-            <li class="navbar_link"><a href="#">Dashboard</a></li>
-            <li class="navbar_link"><a href="#">Admin</a></li>
+            <li class="navbar_link"><a href="registration.php">Registration</a></li>
+            <li class="navbar_link"><a href="dashboard.php">Dashboard</a></li>
+            <li class="navbar_link"><a href="admin.php">Admin</a></li>
         </ul>
         
         <ul id="userprofil">
@@ -18,11 +19,12 @@ $_SESSION['first_name'] = $row['first_name'];
             if(isset($_SESSION["user"])){
                 ?>
                 
-                <li class="profil"><a href="#"><?php print_r($_SESSION['first_name']) ?></a></li>
-                <li class="profil"><a href="doCodes/doLogout.php">Logud</a></li>
+                <li class="profil"><?php print_r($_SESSION['first_name']) ?></li>
+                <li class="profil2"><a href="doCodes/doLogout.php">Logud</a></li>
                 <!--<li class="profil"><a href="doCodes/doLogout.php">Logud</a></li>-->
                 <?php
             }
             ?>
         </ul>
+    </div>
     </nav>
